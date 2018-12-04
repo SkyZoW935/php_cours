d9afe6e5f950cbc7dfa4ddef66b0cba20b637b3c
SkyZoW935 <damien.franchet@imie.fr>
2018-12-04 12:02:22 +0100

    Création d'une class person avec ses instances

--- index.php
+++ index.php
@@ -7,9 +7,19 @@ class Person {
     public function setFirstname($pFirstname){
         $this->firstname = $pFirstname;
     }
+
+    public function getFirstname() {
+        return $this->firstname;
+    }
+
     public function setLastname($pLastname){
         $this->lastname = $pLastname;
     }
+
+    public function getLastname() {
+        return $this->lastname;
+    }
+
 }
 
 $person1 = new Person();
@@ -18,10 +28,14 @@ $person1 = new Person();
 
 $person1->setFirstname("John");
 $person1->setLastname("Doe");
-var_dump($person1);
+var_dump($person1->getFirstname());
+var_dump($person1->getLastname());
 
 //$person2 = new Person();
 //$person2 ->firstname = "Jane";
 //$person2 ->lastname = "Die";
-var_dump($person2);
+
+//$person2->setFirstname("Jane");
+//$person2->setLastname("Die");
+//var_dump($person2);
 
